183e9f8824b13f31ca226715c2868087b0a8bf11
chasoFaktor <lisc@mailbox.org>
2023-01-31 20:40:45 +0100

    oh oh

--- js/appearonview.js
+++ js/appearonview.js
@@ -1,37 +1,29 @@
+(function () {
+  let elements;
+  let windowHeight;
 
+  function init() {
+    elements = document.querySelectorAll(".slideinonview");
+    console.log(elements);
+    windowHeight = window.innerHeight;
+  }
 
-(function() {
-    let elements;
-    let windowHeight;
-    
-  
-    function init() {
-      elements = document.querySelectorAll('.slideinonview');
-      console.log(elements);
-      windowHeight = window.innerHeight;
-    }
-  
-    function checkPosition() {
-        
-        for (let i = 0; i < elements.length; i++) {
-            let element = elements[i];
-            let positionFromTop = elements[i].getBoundingClientRect().top;
-  
-            if (positionFromTop <= windowHeight) {
-        
+  function checkPosition() {
+    for (let i = 0; i < elements.length; i++) {
+      let element = elements[i];
+      let positionFromTop = elements[i].getBoundingClientRect().top;
 
-                element.classList.add('slideinleft');
-                element.classList.remove('hidden');
-        }
+      if (positionFromTop <= windowHeight) {
+        element.classList.add("slideinleft");
+        element.classList.remove("hidden");
       }
     }
-  
-    window.addEventListener('scroll', checkPosition);
-    window.addEventListener('resize', init);
-  
-    init();
-    checkPosition();
-  })();
+  }
 
-  
-  
+  window.addEventListener("scroll", checkPosition);
+  window.addEventListener("resize", init);
+  window.addEventListener("DOMContentLoaded", checkPosition);
+
+  init();
+  checkPosition();
+})();
